c7ead57ef10e760e84025ec1c4d3595d8dcb236f
mariela9709 <mariela0509_alv@live.com.mx>
2022-04-01 17:00:14 -0600

    changes added

--- src/main/java/tony/skeleton/webinteraction/WebInteraction.java
+++ src/main/java/tony/skeleton/webinteraction/WebInteraction.java
@@ -2,7 +2,9 @@ package tony.skeleton.webinteraction;
 
 import java.time.Duration;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import tony.armor.osInteraction.OsInteraction;
 
@@ -25,7 +27,7 @@ public class WebInteraction {
 		else browser.switchTo().frame(browser.findElement(By.xpath(id)));
 	}
 
-	public void clickLink(String name, String locator) {
+	public void inputClick(String name, String locator) {
 		browser.findElement(By.xpath(locator)).click();
 	}
 		
@@ -37,4 +39,14 @@ public class WebInteraction {
 		browser.findElement(By.xpath(locator)).click();
 		browser.findElement(By.xpath(value)).click();
 	}
+	
+	public void selectMultipleOption(String name, String locator, String values) {
+		Actions action=new Actions(browser);
+		action.keyDown(Keys.CONTROL).build().perform();
+		for (String value : values.split(";"))
+		    browser.findElement(By.xpath(locator.replace("REPLACE", value))).click();
+		//browser.findElement(By.xpath(locator2)).click();
+		action.keyUp(Keys.CONTROL).build().perform();
+	}
+	
 }
--- src/test/java/tony/unit/login/PositiveLoginTest.java
+++ src/test/java/tony/unit/login/PositiveLoginTest.java
@@ -13,11 +13,16 @@ public class PositiveLoginTest {
 	webInteraction.openBrowser("chrome");
 	webInteraction.navigateToPage("https://codepen.io/anon/pen/XgzJmQ");
 	webInteraction.changeFrame("//iframe[contains(@id, 'index.html-')]");
-	webInteraction.clickLink("author link", "//a[contains(., 'lorem.in.th')]");
+	webInteraction.inputClick("author link", "//a[contains(., 'lorem.in.th')]");
 	webInteraction.setText("email", "//input[@id='input-type-email']","mariela0509_alv@live.com.mx");
 	webInteraction.setText("password", "//input[@id='input-type-password']","password");
 	webInteraction.selectDropDownValue("select box", "//select[@id='input-select']", "//select[@id='input-select']/optgroup[@label='Group1']/option[.='2']");
 	webInteraction.setText("input text", "//input[@id='input-type-text']","Lorem ipsum dolor sit amet, consectetur");
+	webInteraction.setText("search","//input[@id='input-type-search']", "search");
+	webInteraction.setText("url","//input[@id='input-type-url']", "https://codepen.io/anon/pen/XgzJmQ");
+	webInteraction.inputClick("radio", "//input[@type='radio'][@name='input-radio'][@value='2']");
+	webInteraction.selectMultipleOption("multiple option", "//select[@id='input-select-multiple']/option[.='REPLACE']", "1;3;2");
+	webInteraction.inputClick("normal button", "//button[text()='Normal button']");
 	webInteraction.changeFrame(null);
 	//prueba
 }
