361f63e43eb77b39b09db9474653e8831b3574f8
Danna.lopez <danna.lopeztovar@qualitestgroup.com>
2022-04-07 12:29:59 -0500

    update

--- src/test/java/tony/unit/login/PositiveLoginTest.java
+++ src/test/java/tony/unit/login/PositiveLoginTest.java
@@ -14,7 +14,7 @@ public class PositiveLoginTest {
 	webInteraction.navigateToPage("https://codepen.io/anon/pen/XgzJmQ");
 	webInteraction.changeFrame("//iframe[contains(@id, 'index.html-')]");
 	webInteraction.inputClick("author link", "//a[contains(., 'lorem.in.th')]");
-	webInteraction.setText("email", "//input[@id='input-type-email']","mariela0509_alv@live.com.mx");
+	webInteraction.setText("email", "//input[@id='input-type-email']","itstark@hotmail.com");
 	webInteraction.setText("password", "//input[@id='input-type-password']","password");
 	webInteraction.selectDropDownValue("select box", "//select[@id='input-select']", "//select[@id='input-select']/optgroup[@label='Group1']/option[.='2']");
 	webInteraction.setText("input text", "//input[@id='input-type-text']","Lorem ipsum dolor sit amet, consectetur");
